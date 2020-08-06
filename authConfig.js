f081e641a3f11476a7642836c3c69d27772a0e0a
felipetssilva <felipetssilva@hotmail.com>
2020-08-06 19:52:50 +0200

    Add files via upload

---
+++ authConfig.js
@@ -0,0 +1,6 @@
+export default {
+    'secret': '$2a$08$2YXLGLxG73O.kKLx18jVqOzuwDF./bIvcBeZwaeruaretAPglf5Y2',
+    'expiresIn': '7d',
+};
+
+//"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiaWF0IjoxNTk1MjcxMzY5LCJleHAiOjE1OTU4NzYxNjl9.TZJQhlKvfCB8nlX_rr5hiFIkSfkGwqFWomrmj-t_bDE"
